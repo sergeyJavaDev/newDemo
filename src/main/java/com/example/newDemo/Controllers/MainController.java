63e3bb9bde51d64cc37429bf27c1c809db35542a
sergeyJavaDeve <wodnyi@mail.ru>
2020-12-24 16:09:32 +0500

    newDemo

---
+++ src/main/java/com/example/newDemo/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.newDemo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(@RequestParam(name="name", required=false, defaultValue="World") String text, Model model){
+        model.addAttribute("name", text);
+        return "mainPage";
+    }
+}
